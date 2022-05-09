a5fda92b8ca91311867ba8ce7b88eb188bf9c14f
Anish <sharmaanish256@gmail.com>
2022-05-09 10:29:21 +0530

    Added API to render data instead of static data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import Box from "./components/box"
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>
           CryptoTop10
+         
         </h1>
+        <Box/>
       </header>
-      <Box rank="1" name="Bitcoin" price="40000"/>
-      <Box rank="2" name="Ethereum" price="2800"/>
-      <Box rank="3" name="BNB" price="400"/>
-      <Box rank="4" name="Solana" price="99"/>
-      <Box rank="5" name="XRP" price="0.64"/>
-      <Box rank="6" name="Terra" price="89"/>
-     
     </div>
   );
 }
--- src/components/box.js
+++ src/components/box.js
@@ -1,11 +1,38 @@
-import React from "react";
+
+import React ,{useState, useEffect} from "react";
+import axios from "axios";
+
+
+const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc";
+
+
+
+
 
 function Box(props){
-    return <div className="tile">
-        <h1> #{props.rank}</h1>
-        <h2> {props.name}</h2>
-        <h3> Current Price : ${props.price}</h3>
+        
+
+        const [data,setData] = useState();
+
+        useEffect(()=>{ axios(`${url}`).then(res => {
+                const stats = res.data.slice(0,10);
+                console.log(stats);
+                setData(stats)
+            });
+            }
+        ,[]); 
+
+        
+
+    return <div>
+        {data?.map((each)=> 
+        <div key={each.market_cap_rank} className="tile">
+            <h1> #{each.market_cap_rank}</h1>
+            <img height="50px" width="50px" src={each.image} alt="logo"></img>
+            <h2> {each.name}</h2>
+            <h3> Current Price : ${each.current_price}</h3>
+        </div>)}
     </div>
-}
+};
 
 export default Box;
